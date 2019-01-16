0036afdfa5efc2cba54659f3d0814bdbcf6dc61b
fbelleau <francois.belleau@gmail.com>
2019-01-15 22:23:40 -0500

    15012

---
+++ bio2rdf_graph.sh
@@ -0,0 +1,12 @@
+python select_type_uri.py http://bio2rdf.org/affymetrix_resource:bio2rdf.dataset.affymetrix.R3 > log_affymetrix &
+python select_type_uri.py http://bio2rdf.org/kegg_resource:bio2rdf.dataset.kegg.R3 > log_kegg &
+python select_type_uri.py http://bio2rdf.org/irefindex_resource:bio2rdf.dataset.irefindex.R3 > log_irefindex &
+python select_type_uri.py http://bio2rdf.org/goa_resource:bio2rdf.dataset.goa.R3 > log_goa &
+python select_type_uri.py http://bio2rdf.org/taxonomy_resource:bio2rdf.dataset.taxonomy.R3 > log_taxonomy &
+python select_type_uri.py http://bio2rdf.org/omim_resource:bio2rdf.dataset.omim.R3 > log_omim &
+python select_type_uri.py http://bio2rdf.org/mgi_resource:bio2rdf.dataset.mgi.R3 > log_mgi &
+python select_type_uri.py http://bio2rdf.org/homologene_resource:bio2rdf.dataset.homologene.R3 > log_homologene &
+python select_type_uri.py http://bio2rdf.org/drugbank_resource:bio2rdf.dataset.drugbank.R3 > log_drugbank &
+python select_type_uri.py http://bio2rdf.org/mesh_resource:bio2rdf.dataset.mesh.R3 > log_mesh &
+python select_type_uri.py http://bio2rdf.org/hgnc_resource:bio2rdf.dataset.hgnc.R3 > log_hgnc &
+python select_type_uri.py http://bio2rdf.org/go_resource:bio2rdf.dataset.go.R3 > log_go &
